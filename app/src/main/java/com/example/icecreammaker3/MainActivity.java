33001e9fae4f2b4d4fe1ae3511027f0ff2167173
pablofrances <pablofrances.n@gmail.com>
2021-02-08 16:11:23 +0100

    v3 final

--- app/src/main/java/com/example/icecreammaker3/MainActivity.java
+++ app/src/main/java/com/example/icecreammaker3/MainActivity.java
@@ -2,6 +2,8 @@ package com.example.icecreammaker3;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
@@ -14,17 +16,14 @@ public class MainActivity extends AppCompatActivity {
 
     EditText editName;
 
-    TextView quantityVar, nameText, creamText;
-    TextView chocolateTV;
-    TextView finalQuantityTV, totalPriceTV;
+    TextView quantityVar, nameText, creamText, chocolateText, quantityText, priceText;
 
     CheckBox creamCheck, chocolateCheck;
 
     int priceU = 3;
     int quantity = 0;
 
-    String name, cream, chocolate, finalQuantity;
-
+    String name, cream, chocolate, quantityString, price;
 
     Toast increaseToast;
     Toast decreaseToast;
@@ -42,21 +41,13 @@ public class MainActivity extends AppCompatActivity {
         quantityVar = (TextView) findViewById(R.id.quantityVar);
         nameText = (TextView) findViewById(R.id.nameText);
         creamText = (TextView) findViewById(R.id.creamText);
-//        cream = creamText.getText().toString();
+        chocolateText = (TextView) findViewById(R.id.chocolateText);
+        quantityText = (TextView) findViewById(R.id.quantityText);
+        priceText = (TextView) findViewById(R.id.priceText);
 
         // CheckBox
         creamCheck = (CheckBox) findViewById(R.id.creamCheck);
         chocolateCheck = (CheckBox) findViewById(R.id.chocolateCheck);
-
-
-        chocolateTV = (TextView) findViewById(R.id.chocolateTV);
-        chocolate = chocolateTV.getText().toString();
-
-        finalQuantityTV = (TextView) findViewById(R.id.finalQuantityTV);
-        finalQuantity = finalQuantityTV.getText().toString();
-
-        // Precio
-        totalPriceTV = (TextView) findViewById(R.id.totalPriceTV);
 
         // Toasts
         increaseToast = Toast.makeText(getApplicationContext(), "No se pueden meter mas helados", Toast.LENGTH_SHORT);
@@ -101,11 +92,11 @@ public class MainActivity extends AppCompatActivity {
 
 
         if (chocolateCheck.isChecked()) {
-            chocolateTV.setText("¿Chocolate?: Si.");
+            chocolate = "¿Chocolate?: Si.";
         } else {
-            chocolateTV.setText("¿Chocolate?: No.");
-
+            chocolate = "¿Chocolate?: No.";
         }
+        chocolateText.setText(chocolate);
     }
 
     public int calculatePrice() {
@@ -116,47 +107,34 @@ public class MainActivity extends AppCompatActivity {
         name = "Nombre: " + editName.getText();
         nameText.setText(name);
         toppings();
-        finalQuantityTV.setText("Cantidad: " + quantity);
-        totalPriceTV.setText("Total: " + calculatePrice() + " €.");
+        quantityString = "Cantidad: " + quantity;
+        quantityText.setText(quantityString);
+        price = "Total: " + calculatePrice() + " €.";
+        priceText.setText(price);
         if (view.getId() == R.id.orderButton) {
             textEmail();
         }
     }
 
-
-//    public void onClickButtonOrder(View view) {
-//        calculatePrice();
-//    }
-
     public void textEmail() {
         EditText editEmail = (EditText) findViewById(R.id.editEmail);
-//        chocolateTextView = (TextView) findViewById(R.id.chocolateTextView);
-//        quantityVarTextView = (TextView) findViewById(R.id.quantityVarTextView);
-//        priceTextView = (TextView) findViewById(R.id.priceTextView);
-//        thxTextView = (TextView) findViewById(R.id.thxTextView);
 
         String[] emails = {editEmail.getText().toString()};
         Log.i("Patata", emails[0]);
 
-        subject = "Nuevo Pedido";
+        String subject = "Nuevo Pedido";
+        String text = name + "\n" + cream + "\n" + chocolate + "\n" + quantityText + "\n" + price;
 
-        chocolate = chocolateTextView.getText().toString();
-        quantityText = quantityVarTextView.getText().toString();
-        price = priceTextView.getText().toString();
-        thx = thxTextView.getText().toString();
-
-        text = name + "\n" + cream + "\n" + chocolate + "\n" + quantityText + "\n" + price + "\n" + thx;
-
-        composeEmail(email, subject, mail);
+        composeEmail(emails, subject, text);
 
     }
 
-    public void composeEmail(String[] email, String subject, String mail) {
+    public void composeEmail(String[] email, String subject, String text) {
         Intent intent = new Intent(Intent.ACTION_SENDTO);
         intent.setData(Uri.parse("mailto:")); // only email apps should handle this
         intent.putExtra(Intent.EXTRA_EMAIL, email[0]);
         intent.putExtra(Intent.EXTRA_SUBJECT, subject);
-        intent.putExtra(Intent.EXTRA_TEXT, mail);
+        intent.putExtra(Intent.EXTRA_TEXT, text);
         if (intent.resolveActivity(getPackageManager()) != null) {
             startActivity(intent);
         }
